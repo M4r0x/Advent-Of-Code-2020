0e8c004d0a11ea72cd2001aecc9a72ec47cba413
M4r0x <74778761+M4r0x@users.noreply.github.com>
2021-06-15 23:50:18 +0200

    theoretically this should be faster since its now presumably actually O(log(n) time complexity (atleast speaking about the binary search executed to find the value pair but might have messed up the actual implementation). Actual time complexity should be O(log(n)+n) "+n" besides log(n) because we still have to iterate through each line in the input file to insert the numbers in the treeset and not just search through the input.

--- src/com/m4r0x/task1/Main.java
+++ src/com/m4r0x/task1/Main.java
@@ -1,5 +1,4 @@
 package com.m4r0x.task1;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -15,14 +14,24 @@ public class Main {
             int i = Integer.parseInt(scan.nextLine());
             redBlackBinaryTree.add(i);
         }
-        for (Integer a : redBlackBinaryTree) {
-            if (redBlackBinaryTree.contains(2020 - a)) {
-                System.out.println(new StringBuilder("The two numbers that add up to 2020 in the given Input are: ").
-                        append(a).append(" and ").append((2020 - a)).append("\nThe product of said 2 integers is: ").
-                        append(a * (2020 - a)));
+        getNumberPairInTreeSetThatAddsToGivenSum(redBlackBinaryTree, 2020);
+        System.out.println(new StringBuilder(Long.toString(System.nanoTime() - startTime)).append(" nanoseconds"));
+    }
+    public static void getNumberPairInTreeSetThatAddsToGivenSum(TreeSet<Integer> input, int sum) throws NullPointerException {
+        int leftPointer = input.first(); // leftPointer starts on the lowest value of the TreeSet
+        int rightPointer = input.last(); // rightPointer starts on the highest value of the TreeSet
+        while (leftPointer + rightPointer != sum) {
+            if (leftPointer + rightPointer < sum) {
+                leftPointer = input.higher(leftPointer); //leftPointer moves to next higher value of the TreeSet
+            } else if (leftPointer + rightPointer > sum) {
+                rightPointer = input.lower(rightPointer); //rightPointer moves to next lower value of the TreeSet
+            } else {
+                System.out.println("There is no given pair numbers in the TreeSet that adds up to the given sum.");
                 break;
             }
         }
-        System.out.println(new StringBuilder(Long.toString(System.nanoTime() - startTime)).append(" nanoseconds"));
+        System.out.println(new StringBuilder("The numbers that add up to 2020 in the given Input are ").
+                append(leftPointer).append(" and ").append(rightPointer).append(" which if multiplied add up to: ").
+                append(leftPointer * rightPointer));
     }
 }
