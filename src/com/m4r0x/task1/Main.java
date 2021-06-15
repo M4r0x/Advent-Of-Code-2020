e29dbb045c2140f5040299db16c4b0ef54d3eb40
m4r0x <m.kraeling03@gmx.de>
2021-06-15 14:21:26 +0200

    Using StringBuilder class instead of "+" Operator to improve performance

--- src/com/m4r0x/task1/Main.java
+++ src/com/m4r0x/task1/Main.java
@@ -4,12 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         long startTime = System.nanoTime();
-        boolean foundNumbers = false;
         File input = new File("input/task1input");
         Scanner scan = new Scanner(input);
         ArrayList<Integer> arrayList = new ArrayList<>();
@@ -17,12 +16,14 @@ public class Main {
             int i = Integer.parseInt(scan.nextLine());
             arrayList.add(i);
         }
-        for(Integer a : arrayList) {
-            if(arrayList.contains(2020-a)) {
-                System.out.println("The two numbers that add up to 2020 in the given Input are: " + a + " and " + (2020 - a) + "\nThe product of said 2 integers is: " + a * (2020 - a));
+        for (Integer a : arrayList) {
+            if (arrayList.contains(2020 - a)) {
+                System.out.println(new StringBuilder("The two numbers that add up to 2020 in the given Input are: ").
+                        append(a).append(" and ").append((2020 - a)).append("\nThe product of said 2 integers is: ").
+                        append(a * (2020 - a)));
                 break;
             }
         }
-        System.out.println(System.nanoTime() - startTime+" nanoseconds");
+        System.out.println(new StringBuilder(Long.toString(System.nanoTime() - startTime)).append(" nanoseconds"));
     }
 }
