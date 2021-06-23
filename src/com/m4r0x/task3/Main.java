11d727f4ecf293c11d8481a724aa7170f06bce50
M4r0x <74778761+M4r0x@users.noreply.github.com>
2021-06-23 20:55:43 +0200

    starting task 3

---
+++ src/com/m4r0x/task3/Main.java
@@ -0,0 +1,16 @@
+package com.m4r0x.task3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        part1v1();
+    }
+    static void part1v1() throws FileNotFoundException {
+        File input = new File("input/task3input");
+    }
+}
